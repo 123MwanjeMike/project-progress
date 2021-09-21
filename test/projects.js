e2ff647d1e6aecfef5dba4b3007e03d0fc277d4c
Mike Mwanje <mwanjemike767@gmail.com>
2021-09-21 15:13:30 +0300

    gets organisation-wide project test

--- test/projects.js
+++ test/projects.js
@@ -13,12 +13,24 @@ jest.mock('@octokit/core', () => ({
   })),
 }));
 
-describe('user tests', () => {
+describe('project tests', () => {
   // user-owned project boards
-  it('gets user-owned projects', (done) => {
+  it('gets user-owned project boards', (done) => {
     chai
       .request(app)
       .get('/projects/user/USERNAME')
+      .then((res) => {
+        expect(res.statusCode).toBe(200);
+        expect(res.body[0]).toHaveProperty('id');
+        done();
+      });
+  });
+
+  // organization-wide project boards
+  it('gets organization-wide project boards', (done) => {
+    chai
+      .request(app)
+      .get('/projects/org/ORGANIZATION_NAME')
       .then((res) => {
         expect(res.statusCode).toBe(200);
         expect(res.body[0]).toHaveProperty('id');
