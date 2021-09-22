6d0e2a28ce475f15c88efdd6af466753f1d7caab
Mike Mwanje <mwanjemike767@gmail.com>
2021-09-22 12:26:51 +0300

    sentry middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import express from 'express';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import configRoutes from './routes';
 
 require('dotenv').config();
 
 const app = express();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({ app }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 app.use(express.json());
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 app.use(express.urlencoded({ extended: true }));
 
 configRoutes(app);
