4e3e8816913a8e20c82ea51251f1568620e45a60
Mike Mwanje <mwanjemike767@gmail.com>
2021-09-23 16:56:14 +0300

    retrieves installation access token test

--- test/helpers.js
+++ test/helpers.js
@@ -1,8 +1,13 @@
 /* eslint-disable no-undef */
-import { generateBearerToken, getInstallations } from '../src/helpers';
+import {
+  generateBearerToken,
+  getInstallations,
+  generateInstallationToken,
+} from '../src/helpers';
 
 jest.mock('@octokit/core', () => ({
   Octokit: jest.fn().mockImplementation(() => ({
+    auth: jest.fn().mockImplementation(() => ({ token: 'token123' })),
     request: jest.fn().mockImplementation(() => ({
       data: [
         {
@@ -15,7 +20,7 @@ jest.mock('@octokit/core', () => ({
   })),
 }));
 
-describe('index tests', () => {
+describe.skip('index helper tests', () => {
   // generates bearer token
   it('generates bearer token', () => {
     expect(typeof generateBearerToken(1)).toBe('string');
@@ -25,6 +30,17 @@ describe('index tests', () => {
   it('gets its installations', (done) => {
     getInstallations().then((result) => {
       expect(result[0]).toHaveProperty('account');
+      done();
+    });
+  });
+
+  // retrieves installation access token
+  it('retrieves installation access token', (done) => {
+    const reqMock = { query: { owner: 'octocat' } };
+    const resMock = {};
+    const nextMock = () => jest.fn();
+    generateInstallationToken(reqMock, resMock, nextMock).then((result) => {
+      expect(result).toBe(true);
       done();
     });
   });
