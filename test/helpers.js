973cd9cd96aa6e067a6494a9664b41d04cfd0c96
Mike Mwanje <mwanjemike767@gmail.com>
2021-09-23 16:51:37 +0300

    gets installations test

--- test/helpers.js
+++ test/helpers.js
@@ -1,9 +1,31 @@
 /* eslint-disable no-undef */
-import { generateBearerToken } from '../src/helpers';
+import { generateBearerToken, getInstallations } from '../src/helpers';
+
+jest.mock('@octokit/core', () => ({
+  Octokit: jest.fn().mockImplementation(() => ({
+    request: jest.fn().mockImplementation(() => ({
+      data: [
+        {
+          id: 1,
+          account: { login: 'octocat', id: 1 },
+          permissions: { metadata: 'read' },
+        },
+      ],
+    })),
+  })),
+}));
 
 describe('index tests', () => {
   // generates bearer token
   it('generates bearer token', () => {
     expect(typeof generateBearerToken(1)).toBe('string');
+  });
+
+  // gets its installations
+  it('gets its installations', (done) => {
+    getInstallations().then((result) => {
+      expect(result[0]).toHaveProperty('account');
+      done();
+    });
   });
 });
