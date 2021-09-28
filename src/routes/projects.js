ffcdd52012ee836626fa9b25bec2920b5cf5be77
Mike Mwanje <mwanjemike767@gmail.com>
2021-09-28 18:01:11 +0300

    router level middleware to assign request 'login' variable

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,9 +1,27 @@
 import express from 'express';
 import Projects from '../controllers/projects';
+import { retrieveInstallationAccessToken } from '../helpers';
 
 const router = express.Router();
 
 router.route('/user/:username').get(Projects.userOwnedPublic);
-router.route('/org/:organization_name').get(Projects.organizationWidePublic);
+
+router.route('/org/:ORGANIZATION_NAME').get(
+  (req, res, next) => {
+    req.login = req.params.ORGANIZATION_NAME;
+    next();
+  },
+  retrieveInstallationAccessToken,
+  Projects.organizationWidePublic,
+);
+
+router.route('/repo/').get(
+  (req, res, next) => {
+    req.login = req.query.owner;
+    next();
+  },
+  retrieveInstallationAccessToken,
+  Projects.repositoryProjects,
+);
 
 module.exports = router;
