22e4757d16f602d0ae7e7142e58196ff34bcd8dd
Mike Mwanje <mwanjemike767@gmail.com>
2021-10-13 15:29:44 +0300

    return user access tokens for right requests test

--- test/auth.js
+++ test/auth.js
@@ -5,7 +5,25 @@ import app from '../src';
 
 chai.use(chaiHttp);
 
+// Mock @octokit/core
+jest.mock('@octokit/core', () => ({
+  Octokit: jest.fn().mockImplementation(() => ({
+    request: jest.fn().mockImplementation(() => ({
+      data: {
+        access_token: 'ghu_123456789',
+        expires_in: 28800,
+        refresh_token: 'ghr_123456789',
+        refresh_token_expires_in: 15811200,
+        scope: '',
+        token_type: 'bearer',
+      },
+    })),
+  })),
+}));
+
 describe('auth tests', () => {
+  let state = null;
+
   // request user identity
   it('requests user identity', (done) => {
     chai
@@ -20,6 +38,8 @@ describe('auth tests', () => {
         expect(res.headers.location).toContain(
           'https://github.com/login/oauth/authorize/?client_id=',
         );
+        const urlSearchParams = new URLSearchParams(res.headers.location);
+        state = Object.fromEntries(urlSearchParams.entries()).state;
         done();
       });
   });
@@ -28,10 +48,23 @@ describe('auth tests', () => {
   it('aborts identify requests made by third parties', (done) => {
     chai
       .request(app)
-      .get('/auth/success/?code=123&state=123')
+      .get('/auth/success/?code=123&state=something_unexpected')
       .then((res) => {
         expect(res.statusCode).toBe(401);
         expect(res.body.error).toBe('Request was created by a third party!');
+        done();
+      });
+  });
+
+  // returns user access tokens for right requests
+  it('return user access tokens for right requests', (done) => {
+    chai
+      .request(app)
+      .get(`/auth/success/?code=123&state=${state}`)
+      .then((res) => {
+        expect(res.statusCode).toBe(200);
+        expect(res.body.token).toBe('ghu_123456789');
+        expect(res.body.message).toBe('Login successful');
         done();
       });
   });
